7c01821ba2f274a681f7356b8ce7942955349e72
lmcarmona2022 <luismcarmona2020@gmail.com>
2022-07-18 00:23:54 -0300

    4 files modified, 2 added, 1 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import {AppComponent } from './app.component';
 import {HeaderComponent} from './header.component';
+import {MainComponent} from './main.component';
 
 @NgModule({
   declarations: [
-    AppComponent, HeaderComponent
+    AppComponent, HeaderComponent, MainComponent
   ],
   imports: [
     BrowserModule
